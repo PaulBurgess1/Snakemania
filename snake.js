d4d9a9de942f7447a36240a06989bf7425c63951
Erwin <erwin_guczuly@outlook.com>
2020-03-01 17:09:19 -0500

    added nav bar (not done)

--- snake.js
+++ snake.js
@@ -1,14 +1,20 @@
 
-function startGame() {
-    myGameArea.start();
-}
+// function startGame() {
+//     myGameArea.start();
+// }
 
-var myGameArea = {
-    canvas : document.createElement("canvas"),
-    start : function() {
-        this.canvas.width = 960;
-        this.canvas.height = 540;
-        this.context = this.canvas.getContext("2d");        
-        document.body.insertBefore(this.canvas, document.body.childNodes[0]);
-    }
+// var myGameArea = {
+//     canvas : document.createElement("canvas"),
+//     start : function() {
+//         this.canvas.width = 960;
+//         this.canvas.height = 540;
+//         this.context = this.canvas.getContext("2d");        
+//         document.body.insertBefore(this.canvas, document.body.childNodes[0]);
+//     }
+// }
+
+
+function drawCanvas() {
+    // Get our Canvas element
+    surface = document.getElementById("myCanvas");
 }
