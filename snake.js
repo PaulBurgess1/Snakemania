eea3c3e0116b18db5c16524857534abe9eece0d9
Erwin <erwin_guczuly@outlook.com>
2020-03-03 15:36:51 -0500

    fixed leaderboard

--- snake.js
+++ snake.js
@@ -1,20 +1,14 @@
-
-// function startGame() {
-//     myGameArea.start();
-// }
-
-// var myGameArea = {
-//     canvas : document.createElement("canvas"),
-//     start : function() {
-//         this.canvas.width = 960;
-//         this.canvas.height = 540;
-//         this.context = this.canvas.getContext("2d");        
-//         document.body.insertBefore(this.canvas, document.body.childNodes[0]);
-//     }
-// }
-
-
 function drawCanvas() {
     // Get our Canvas element
     surface = document.getElementById("myCanvas");
 }
+
+// window.onresize = function(event) 
+// {
+//     // test if one element overlaps another
+//     if($('#div1').overlaps('#div2'))
+//     {
+//         //Do stuff, like hide one of the overlapping divs
+//         $('#div1').hide();
+//     }
+// }
