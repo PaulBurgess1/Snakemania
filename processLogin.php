18866ded0f2d052e19666d4eea7f8d8493d409dc
bbrayan <30880116+bbrayan@users.noreply.github.com>
2020-04-17 17:09:05 -0400

    almost done this part

--- processLogin.php
+++ processLogin.php
@@ -12,5 +12,24 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
     if (empty($userName)){
         $password_error=" |username field was empty| ";
     }
+    if($userName_error == '' and $password_error == ''){
+        unset($_POST['submit']);
+        $search="SELECT password FROM snakemaniadb WHERE username='$userName';";
+        if ($result=mysqli_query($conn,$search)){
+            $q_rows = mysqli_num_rows($result);
+            if ($q_rows==0){
+                $userName_error=" |username does not exist| ";
+            }else{
+                $row = mysqli_fetch_assoc($result);
+                if(password_verify($password, $row['password'])){
+                    //gotta talk with erwin on this part
+                    echo "done";
+                }
+                else{
+                    $password_error=" |password was incorrect| ";
+                }
+            }
+        }
+    }
 }
 ?>
